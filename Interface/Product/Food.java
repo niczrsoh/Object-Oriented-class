94c47a85085546a55a8a912a01a0d4b8275f01ab
niczrsoh <87169349+niczrsoh@users.noreply.github.com>
2022-06-05 23:12:00 +0800

    add Interface implementation

---
+++ Interface/Product/Food.java
@@ -0,0 +1,16 @@
+package Interface.Product;
+
+public class Food extends Product{
+    private int calories;
+    Food(String description,double price, int calories) {
+        this.description = description;
+        this.price = price;
+        this.calories = calories;
+    }
+    int getCalories() {
+        return calories;
+    }
+    public String display(){
+        return "Food name: "+getDescription()+String.format("\nFood price: %.2f",getPrice())+"\nFood calories: "+getCalories()+"\n";
+    }
+}
